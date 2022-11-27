57d30effddc8ca584cebe38dad00596c39224748
hasibprangon <hasibulhossainp09@gmail.com>
2022-11-26 21:08:47 -0800

    Updated code style for better consistency

--- StudentList.java
+++ StudentList.java
@@ -90,28 +90,38 @@ public class StudentList {
 			}
 
 			System.out.println("Data Loaded.");				
-		}
-		else if(args[0].contains("c")) 
-		{
-			System.out.println("Loading data ...");			
+		} else if (args[0].contains("c")) {
+
+			System.out.println("Loading data ...");
+
 			try {
-			BufferedReader s = new BufferedReader(
-					new InputStreamReader(
-							new FileInputStream("students.txt"))); 
-			String D = s.readLine();
-			char a[] = D.toCharArray();			
-			boolean in_word = false;
-			int count=0;
-			for(char c:a) {
-				if(c ==' ') 
-				{
-					if (!in_word) {	count++; in_word =true;	}
-					else { in_word=false;}			
+				BufferedReader s = new BufferedReader(
+						new InputStreamReader(
+								new FileInputStream("students.txt")));
+				String D = s.readLine();
+				char a[] = D.toCharArray();
+				boolean in_word = false;
+				int count = 0;
+
+				for (char c : a) {
+					if (c == ' ') {
+						if (!in_word) {
+							count++;
+							in_word = true;
+						} else {
+							in_word = false;
+						}
+					}
 				}
+
+				System.out.println(count + " word(s) found " + a.length);
+
+			} catch (Exception e) {
+
 			}
-			System.out.println(count +" word(s) found " + a.length);
-			} catch (Exception e){} 
-			System.out.println("Data Loaded.");				
+
+			System.out.println("Data Loaded.");
+
 		}
 	}
 }
